0ebfc71484ad35dc37321ae5e632edb995d674a0
Sepehr Sobhani <sepehr411@gmail.com>
2024-03-08 19:57:48 -0500

    Remove annoying seach

--- init.lua
+++ init.lua
@@ -262,18 +262,60 @@ require('lazy').setup {
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    ---@type Flash.Config
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash search forwards" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").jump({
-        search = { forward = false, wrap = false, multi_window = false },
-      }) end, desc = "Flash search backwards" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash search forwards',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump {
+            search = { forward = false, wrap = false, multi_window = false },
+          }
+        end,
+        desc = 'Flash search backwards',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
+    },
+    config = function()
+      require('flash').setup {
+        modes = {
+          search = {
+            enabled = false,
+          },
+        },
+      }
+    end,
   },
   { 'github/copilot.vim' },
   -- NOTE: Plugins can also be added by using a table,
