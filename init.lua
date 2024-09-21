11890c327145c434ea4f2b6c44ca60b0ae850296
Sepehr Sobhani <sepehr411@gmail.com>
2024-09-21 09:31:46 -0400

    Fix keymaps

--- init.lua
+++ init.lua
@@ -292,23 +292,24 @@ require('lazy').setup {
     event = 'VeryLazy',
     config = function()
       require('chatgpt').setup()
-      require('which-key').register({
-        l = {
-          name = 'ChatGPT',
-          l = { '<cmd>ChatGPT<CR>', 'ChatGPT' },
-          e = { '<cmd>ChatGPTEditWithInstruction<CR>', 'Edit with instruction', mode = { 'n', 'v' } },
-          g = { '<cmd>ChatGPTRun grammar_correction<CR>', 'Grammar Correction', mode = { 'n', 'v' } },
-          t = { '<cmd>ChatGPTRun translate<CR>', 'Translate', mode = { 'n', 'v' } },
-          k = { '<cmd>ChatGPTRun keywords<CR>', 'Keywords', mode = { 'n', 'v' } },
-          d = { '<cmd>ChatGPTRun docstring<CR>', 'Docstring', mode = { 'n', 'v' } },
-          a = { '<cmd>ChatGPTRun add_tests<CR>', 'Add Tests', mode = { 'n', 'v' } },
-          o = { '<cmd>ChatGPTRun optimize_code<CR>', 'Optimize Code', mode = { 'n', 'v' } },
-          s = { '<cmd>ChatGPTRun summarize<CR>', 'Summarize', mode = { 'n', 'v' } },
-          f = { '<cmd>ChatGPTRun fix_bugs<CR>', 'Fix Bugs', mode = { 'n', 'v' } },
-          x = { '<cmd>ChatGPTRun explain_code<CR>', 'Explain Code', mode = { 'n', 'v' } },
-          r = { '<cmd>ChatGPTRun roxygen_edit<CR>', 'Roxygen Edit', mode = { 'n', 'v' } },
+      require('which-key').add {
+        {
+          mode = { 'n', 'v' },
+          { '<leader>l', group = 'ChatGPT' },
+          { '<leader>la', '<cmd>ChatGPTRun add_tests<CR>', desc = 'Add Tests' },
+          { '<leader>ld', '<cmd>ChatGPTRun docstring<CR>', desc = 'Docstring' },
+          { '<leader>le', '<cmd>ChatGPTEditWithInstruction<CR>', desc = 'Edit with instruction' },
+          { '<leader>lf', '<cmd>ChatGPTRun fix_bugs<CR>', desc = 'Fix Bugs' },
+          { '<leader>lg', '<cmd>ChatGPTRun grammar_correction<CR>', desc = 'Grammar Correction' },
+          { '<leader>lk', '<cmd>ChatGPTRun keywords<CR>', desc = 'Keywords' },
+          { '<leader>ll', '<cmd>ChatGPT<CR>', desc = 'ChatGPT' },
+          { '<leader>lo', '<cmd>ChatGPTRun optimize_code<CR>', desc = 'Optimize Code' },
+          { '<leader>lr', '<cmd>ChatGPTRun roxygen_edit<CR>', desc = 'Roxygen Edit' },
+          { '<leader>ls', '<cmd>ChatGPTRun summarize<CR>', desc = 'Summarize' },
+          { '<leader>lt', '<cmd>ChatGPTRun translate<CR>', desc = 'Translate' },
+          { '<leader>lx', '<cmd>ChatGPTRun explain_code<CR>', desc = 'Explain Code' },
         },
-      }, { prefix = '<leader>' })
+      }
     end,
     dependencies = {
       'MunifTanjim/nui.nvim',
@@ -439,12 +440,15 @@ require('lazy').setup {
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
       }
     end,
   },
